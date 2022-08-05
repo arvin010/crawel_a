8dd38812a92a97a839abf3e5ace73d3aa52c735e
arvin010 <svip5285@outlook.com>
2022-08-05 20:04:39 +0800

    a

---
+++ StartCore.py
@@ -0,0 +1,128 @@
+from util.Database import Database
+from util.ConfigUtil import Config
+from core.CoreThread import CoreThread
+import time
+import datetime
+from random import *
+
+
+class StartCore():
+    __max_tharead_number = 0
+    __idle_tharead_number = 0
+    __busy_tharead_number = 0
+    __tharead_data = []
+    max_run_time = 60 * 0.5
+
+    def __init__(self, max_tharead_number=None):
+        self.config = Config()
+        self.__linkDb()
+        if max_tharead_number == None:
+            self.__max_tharead_number = int(self.config.getThreadConfig('max_keywords_thread'))
+        else:
+            self.__max_tharead_number = int(max_tharead_number)
+
+    def __linkDb(self):
+        self.db = Database(
+            host=self.config.getMysqlConfig('host'),
+            user=self.config.getMysqlConfig('user'),
+            password=self.config.getMysqlConfig('password'),
+            db=self.config.getMysqlConfig('db'),
+        )
+
+    def getClientStartState(self):
+        sql = "select id from customer where state = %s and is_del = %s and monitoring_state = %s"
+        result = self.db.select(sql, data=(0, 0, 0))
+        client_id_list = {}
+        if len(result) > 0:
+            for val in result:
+                client_id_list[val[0]] = {
+                    'client_id': val[0],
+                }
+        return client_id_list
+
+    def getKeywordsAuto(self, client_id_list):
+        result = []
+        if len(client_id_list) > 0:
+            sql = 'select a.id,a.keywords,b.cookie,b.uuid,b.latitude,b.longitude,a.max_price,a.min_price,a.client_id,a.auto_create_shopping_car,b.default_addr_id,a.time_where from keywords as a left join operation_user as b on a.operation_user_id = b.id where a.is_del=%s and (a.state in %s or (a.state = 1 and (UNIX_TIMESTAMP(now()) - UNIX_TIMESTAMP(a.last_start_time)) >= 25)) and error_num < %s and a.client_id in %s order by a.thread_end_time asc limit {}'.format(
+                self.__idle_tharead_number)
+            result = self.db.select(sql, data=(0, [0, 2, 3], 3, client_id_list))
+        return result
+
+    def startAuto(self):
+        while True:
+            self.__idle_tharead_number = self.__max_tharead_number - len(self.__tharead_data)
+            self.__busy_tharead_number = len(self.__tharead_data)
+
+            if self.__idle_tharead_number > 0:
+                self.addTask()
+
+            if self.__busy_tharead_number > 0:
+                self.cleanTask()
+
+            time.sleep(26)
+
+    def cleanTask(self):
+        for task_key, task_obj in enumerate(self.__tharead_data):
+            if task_obj.is_alive() == False:
+                del self.__tharead_data[task_key]
+
+    def getCrawlUser(self, client_id_list):
+        sql = "SELECT id,cookie,uuid,latitude,longitude,client_id FROM operation_user where client_id in %s and user_type=%s and is_del=%s"
+        result = self.db.select(sql, data=(client_id_list, 1, 0))
+        data_info = {}
+        for val in result:
+            if (val[5] not in data_info):
+                data_info[val[5]] = []
+            data_info[val[5]].append({
+                'id': val[0],
+                'cookie': val[1],
+                'uuid': val[2],
+                'latitude': val[3],
+                'longitude': val[4],
+            })
+        return data_info
+
+    def addTask(self):
+        client_data = self.getClientStartState()
+        if len(client_data) > 0:
+            client_id_list = list(client_data.keys())
+            keywords_data = self.getKeywordsAuto(client_id_list)
+            if len(keywords_data) > 0:
+                data_info = self.getCrawlUser(client_id_list)
+                for keywords_val in keywords_data:
+                    info = {
+                        'id': keywords_val[0],
+                        'keywords': keywords_val[1],
+                        'cookie': keywords_val[2],
+                        'uuid': keywords_val[3],
+                        'latitude': keywords_val[4],
+                        'longitude': keywords_val[5],
+                        'max_price': keywords_val[6],
+                        'min_price': keywords_val[7],
+                        'client_id': keywords_val[8],
+                        'auto_create_shopping_car': keywords_val[9],
+                        'default_addr_id': keywords_val[10],
+                        'crawl_user_info': self.randomUser(data_info, keywords_val[8]),
+                        'time_where': keywords_val[11]
+                    }
+
+                    tk = CoreThread("keywords_id:".format(keywords_val[0]), info, self.max_run_time)
+                    self.saveAutoStartData(info['id'])
+                    self.__tharead_data.append(tk)
+                    tk.start()
+            else:
+                print("没有要处理的数据")
+        else:
+            print("没有要处理的数据")
+
+    def saveAutoStartData(self, id):
+        data = (1, datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), id)
+        sql = "update keywords set state=%s,last_start_time=%s where id=%s"
+        try:
+            self.db.update(sql, data=data)
+        except Exception as res:
+            self.__linkDb()
+            self.db.update(sql, data=data)
+
+    def randomUser(self, data_info, client_id):
+        return data_info[client_id][randint(0, len(data_info[client_id])) - 1]
--- test_read_csv.py
+++ test_read_csv.py
@@ -1,9 +1,12 @@
 import csv
 
-
-
-
 target_path=r"D:\work\amazon\易仓AM02-美国\US\2022-8-01_2022-8-3_1659680076.9159417_MonthlyTransaction_US.csv"
+
+with open(target_path,'rt', newline='', encoding='utf-8', errors='ignore') as f:
+    lines = open(target_path,encoding='utf-8', errors='ignore').readlines()
+    open(target_path.replace("-","_"),'w', newline='', encoding='utf-8', errors='ignore').writelines(lines[6:])
+
+
 with open(target_path,  'rt', newline='', encoding='utf-8', errors='ignore') as f:
     reader = csv.reader(f)
     print(type(reader))
@@ -13,3 +16,39 @@ with open(target_path,  'rt', newline='', encoding='utf-8', errors='ignore') as 
 
     for row in reader:
         print(row[0])
+
+
+
+
+import pandas as pd
+from io import StringIO
+# data = pd.read_csv(target_path)
+# print (data)
+
+df = pd.read_csv(target_path, sep='\t',index_col=False,header=None)
+
+print(type(df))
+print(df)
+row7=df.iloc[7:8]
+print(row7.values.tolist()[0][0])
+# df_new=df.iloc[7:]
+# df_new=df_new.reset_index(drop=True)  # 重置index
+# print(1111111111111,list(row7))
+# df_new.columns = list(row7)
+# # df.rename(index=str.lower, columns=str.upper)
+# print(df_new)
+# print (data.head())
+
+# print("*"*100)
+# print(row7)
+
+
+
+df = pd.DataFrame(data= [[1, 2, 3],[4, 5, 6], [7, 8, 9]], index=['e', 'f', 'g'], columns=['a','b','c'])
+print(df.iloc[1:2,:])
+
+
+data = ('a,b,c\n1,Yes,2\n3,No,4')
+d=pd.read_csv(StringIO(data),
+            true_values=['Yes'], false_values=['No'])
+print(d)
